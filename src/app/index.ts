2be01ae37a0f73ece7061a61c5cea5ec88709c43
Saravanakumar <saravanakumargn@gmail.com>
2016-12-25 12:44:08 +0530

    app change

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {routing, RootComponent} from './routes';
+import { FormsModule }   from '@angular/forms';
 
 import {CaseConvertComponent} from './case-convert/case-convert.component';
+
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   imports: [
     BrowserModule,
+    ClipboardModule,
+    FormsModule,
     routing
   ],
   declarations: [
